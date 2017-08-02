26dbef7c9d84275891e52f11efd49c5049afa8fe
Gabriel Erdelyi <erdgabios@gmail.com>
2017-08-02 15:58:01 +0200

    lesson6 finish segue passing variables between views

---
+++ swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  swoosh
+//
+//  Created by G on 01/08/2017.
+//  Copyright © 2017 erdgabios. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String!
+    var selectedSkillLevel: String!
+}
